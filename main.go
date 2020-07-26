0b2840f286ec53ff49f9519d17d62ff6778567de
Alberto Sanchez <slalbertojesus@gmail.com>
2020-07-26 00:09:12 -0500

    Se añadio servidor simple en go

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import "fmt"
+import "github.com/gofiber/fiber"
+import "os"
+import "log"
+import "net/http"
+import "github.com/PuerkitoBio/goquery"
+import "github.com/gofiber/template/html"
+
+func errorHandling(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func main() {
+
+	engine := html.New(".", ".html")
+
+	app := fiber.New(&fiber.Settings{
+		Views: engine,
+	})
+	app.Get("/home", func(c *fiber.Ctx) {
+		_ = c.Render("buscador", fiber.Map{})
+	})
+
+	query := "tarjeta de video"
+	mercadoLibreUrl := "https://listado.mercadolibre.com.mx/"
+	input := "#D[A:" + query + "]"
+	busqueda := mercadoLibreUrl + query + input
+	response, error := http.Get(busqueda)
+	fmt.Println(busqueda)
+	errorHandling(error)
+	fmt.Println(response)
+	fmt.Println(response.Body)
+	defer response.Body.Close()
+
+	// Create a goquery document from the HTTP response
+	document, err := goquery.NewDocumentFromReader(response.Body)
+	if err != nil {
+		log.Fatal("Error loading HTTP response body. ", err)
+	}
+
+	document.Find("img").Each(func(index int, element *goquery.Selection) {
+		imgSrc, exists := element.Attr("src")
+		if exists {
+			fmt.Println(imgSrc)
+		}
+	})
+	app.Listen(3000)
+}
